91875719a84ee5cddeb4adf19434cda1a63ac5a5
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-12 23:05:13 -0600

    Initial Commit

---
+++ colors array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  colors array
+//
+//  Created by Lingyi Zhan on 12/12/20.
+//  Copyright © 2020 Lingyi Zhan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
